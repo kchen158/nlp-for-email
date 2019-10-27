56d8fffbec284d7b6deeed70a312b8338c7766a8
kchen158 <56097580+kchen158@users.noreply.github.com>
2019-10-26 18:24:38 -0700

    Update dataprocess.py

--- dataprocess.py
+++ dataprocess.py
@@ -1,16 +1,24 @@
 import os
 import os.path
-
+import nltk
+from nltk.tokenize import sent_tokenize
 filedirham='/Users/kejunchen/Desktop/hamdebug/'
 filenames=os.listdir(filedirham)
+file=open('ham_training.txt','w')
 for filename in filenames:
-    f = open(filedirham+filename, 'r')
-    print(f.read())
-
+    filepath=filedirham+filename
+    f = open(filepath, 'r')
+    for line in open(filepath):
+        file.writelines(line)
+    file.write('\n')
+file.close
+file=open('spam_training.txt','w')
 filedirspam='/Users/kejunchen/Desktop/spamdebug/'
 filenames=os.listdir(filedirspam)
 for filename in filenames:
-    f = open(filedirspam+filename, 'r')
-    print(f.read())
-
-
+    filepath=filedirspam+filename
+    f = open(filepath, 'r')
+    for line in open(filepath):
+        file.writelines(line)
+    file.write('\n')
+file.close
